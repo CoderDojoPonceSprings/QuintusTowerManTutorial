e058f8e1264a507edd657f11bd3711a6baf6001a
Josh Gough <jsgough@gmail.com>
2014-03-16 20:57:20 -0400

    And, it works!

--- Josh/tower_man.js
+++ Josh/tower_man.js
@@ -1,34 +1,39 @@
-window.addEventListener("load", function() {
-  // Setup Quintus
-  var Q = window.Q = Quintus(
-    { development: true })
-    .include("Sprites, Scenes, Input, 2D")
-    .setup({ width: 640, height: 480 });
-    
-  // Key and joy controls
+// 1. Wait for the onload event
+window.addEventListener("load",function() {
+
+  // 2. Set up a basic Quintus object
+  //    with the necessary modules and controls
+  //    to make things easy, we're going to fix this game at 640x480
+  var Q = window.Q = Quintus({ development: true })
+          .include("Sprites, Scenes, Input, 2D")
+          .setup({ width: 640, height: 480 });
+
+  // 3. Add in the default keyboard controls
+  //    along with joypad controls for touch
   Q.input.keyboardControls();
   Q.input.joypadControls();
-  
-  // Basic sprite
+
+  // 4. Add in a basic sprite to get started
   Q.Sprite.extend("Player", {
     init: function(p) {
-      this._super(p, {
+
+      this._super(p,{
         sheet:"player"
       });
-      
+
       this.add("2d");
     }
   });
-  
-  // Minimal level
-  Q.scene("level1", function(stage) {
-    var player = stage.insert(
-      new Q.Player({ x: 48, y: 48}));
+
+  // 5. Put together a minimal level
+  Q.scene("level1",function(stage) {
+    var player = stage.insert(new Q.Player({ x: 48, y: 48 }));
   });
-  
-  // Load and start the level
-  Q.load("http://html5gametutorial.com/tower_man/images/sprites.png, http://html5gametutorial.com/tower_man/data/sprites.json, level.json", function() {
-    Q.load("http://html5gametutorial.com/tower_man/images/sprites.png", "http://html5gametutorial.com/tower_man/data/sprites.json");
+
+  // 6. Load and start the level
+  Q.load("sprites.png, sprites.json, level.json", function() {
+    Q.compileSheets("sprites.png","sprites.json");
     Q.stageScene("level1");
   });
+
 });
